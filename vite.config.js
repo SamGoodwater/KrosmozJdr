6625c9d98ecd2a380824a4de83a042d6ed7d1076
Goodwater <44548271+SamGoodwater@users.noreply.github.com>
2025-04-01 13:55:19 +0200

    Maj vers php 8.4

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
-import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "path";
 
 export default defineConfig({
     plugins: [
-        tailwindcss(),
         laravel({
             input: "resources/js/app.js",
             ssr: "resources/js/ssr.js",
